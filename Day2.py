351396c88f4cdf0278eae78ec4e698fbea9064cd
Alexandre Hassan <alexhassan@live.ca>
2020-12-14 13:29:01 -0500

    Question 2.2 done

--- Day2.py
+++ Day2.py
@@ -30,7 +30,7 @@ How many passwords are valid according to their policies?
 """
 
 
-def count_valid_passwords(filepath):
+def count_valid_passwords_sled(filepath):
     f = open(filepath, "r")
     lines = f.readlines()
     count_of_valid = 0
@@ -40,12 +40,53 @@ def count_valid_passwords(filepath):
         upper_bound = int(line.split("-")[1].split(":")[0].split(" ")[0])
         letter = line.split("-")[1].split(":")[0].split(" ")[1].strip()
         password = line.split("-")[1].split(":")[1].strip()
-        if password.count(letter) in range(lower_bound, upper_bound+1):
+        if password.count(letter) in range(lower_bound, upper_bound + 1):
             count_of_valid += 1
 
     print(str(count_of_valid))
     return count_of_valid
 
 
+"""
+--- Part Two ---
+
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate 
+Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the 
+sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second 
+character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these 
+positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy 
+enforcement.
+
+Given the same example list from above:
+
+    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+How many passwords are valid according to the new interpretation of the policies?
+"""
+
+
+def count_valid_passwords_Toboggan(filepath):
+    f = open(filepath, "r")
+    lines = f.readlines()
+    count_of_valid = 0
+
+    for line in lines:
+        first_position = int(line.split("-")[0])
+        second_position = int(line.split("-")[1].split(":")[0].split(" ")[0])
+        letter = line.split("-")[1].split(":")[0].split(" ")[1].strip()
+        password = line.split("-")[1].split(":")[1].strip()
+        if bool(password[first_position-1] == letter) ^ bool(password[second_position-1] == letter):
+            count_of_valid += 1
+
+    return count_of_valid
+
+
 if __name__ == '__main__':
-    count_valid_passwords("Files/input_Day2.txt")
+    count_valid_passwords_sled("Files/input_Day2.txt")
+    count_valid_passwords_Toboggan("Files/input_Day2.txt")
