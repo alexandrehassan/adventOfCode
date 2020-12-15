e9849e9ffc4bbf4618aca552a832c998ea76ea6a
Alexandre Hassan <alexhassan@live.ca>
2020-12-15 16:43:44 -0500

    3.2 done and confirmed

--- Day3.py
+++ Day3.py
@@ -68,18 +68,56 @@ encounter?
 
 
 def number_of_trees_3_1(filename) -> int:
+    return number_of_trees(filename, 3, 1)
+
+
+"""
+--- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left 
+corner and traverse the map all the way to the bottom: 
+
+    Right 1, down 1.
+    Right 3, down 1. (This is the slope you already checked.)
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, 
+these produce the answer 336. 
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+"""
+
+
+def number_of_trees(filename: str, x_slope: int, y_slope: int) -> int:
     lines = open(filename, "r").readlines()
     x = 0
     trees = 0
-    for line in lines:
-        line = line.strip()
+    line_number = 0
+    while line_number < len(lines):
+        line = lines[line_number].strip()
         if line[x] == '#':
             trees += 1
-        x += 3
+        x += x_slope
         if x >= len(line):
             x = x - len(line)
+        line_number += y_slope
     return trees
 
 
+def solve_number_2(filename):
+    file = filename
+    slope_1_1 = number_of_trees(file, 1, 1)
+    slope_3_1 = number_of_trees(file, 3, 1)
+    slope_5_1 = number_of_trees(file, 5, 1)
+    slope_7_1 = number_of_trees(file, 7, 1)
+    slope_1_2 = number_of_trees(file, 1, 2)
+    return slope_1_1 * slope_3_1 * slope_5_1 * slope_7_1 * slope_1_2
+
+
 if __name__ == '__main__':
-    print(int(number_of_trees_3_1("Files/input_Day3.txt")))
+    print(number_of_trees_3_1("Files/input_Day3.txt"))
+    print(solve_number_2("Files/input_Day3.txt"))
